7f655daa14812e721bd0ad60929e826c6506e525
Timo Lommen <dytigames@gmail.com>
2020-01-23 11:09:55 +0100

    styling login

--- login.php
+++ login.php
@@ -9,20 +9,18 @@
             <?php include('inc/errors.php'); ?>
             
             <div class="input-group">
-                <label>Gebruikersnaam</label>
-                <input type="text" name="username" >
+                <input placeholder="Gebruikersnaam" type="text" name="username" >
             </div>
             
             <div class="input-group">
-                <label>Wachtwoord</label>
-                <input type="password" name="password">
+                <input placeholder="Wachtwoord" type="password" name="password">
             </div>
             
             <div class="input-group">
                 <button type="submit" class="btn" name="login_user">Inloggen</button>
             </div>
             
-            <p>Nog geen account? <a href="registreer.php">Registreer</a></p>
+            <p class="form-help">Nog geen account? <a href="registreer.php">Registreer</a></p>
         </form>
     </main>
 
